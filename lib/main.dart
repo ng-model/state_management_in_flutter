9a36a87c9defc6c4b96aeffa9247f9bc3985cb67
Manoj <manoj@Turtle-Jr-II.local>
2019-03-02 21:19:38 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:statemanagement/screens/MyHomePage.dart';
+void main() => runApp(new MyApp());
+class MyApp extends StatefulWidget {
+  
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+class _MyAppState extends State<MyApp> {
+  int counter;
+  @override
+  void initState() {
+    super.initState();
+    counter = counter ?? 0;
+  }
+  void _decrementCounter(_) {
+    setState(() {
+      counter--;
+      print('decrement: $counter');
+    });
+  }
+  
+  void _incrementCounter(_) {
+    setState(() {
+      counter++;
+      print('increment: $counter');
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(
+        title: 'My Home Page',
+        counter: counter,
+        decrementCounter: _decrementCounter,
+        incrementCounter: _incrementCounter,
+      ),
+    );
+  }
+}
